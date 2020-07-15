f44cc632389fd4189d68a9bf0b1ab15999e6e2ba
Somesh Patel <56200611+im-somax@users.noreply.github.com>
2020-07-16 01:54:11 +0530

    Added Data class

--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
 class QuizQuestionActivity : AppCompatActivity() {
 
@@ -9,6 +10,18 @@ class QuizQuestionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
 
-        val questionslist = Constants.getQuestions()
+        val questionsList = Constants.getQuestions()
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition-1]
+        progressBar.progress = currentPosition
+        tv_progess.text = "$currentPosition" + "/" + progressBar.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
     }
 }
